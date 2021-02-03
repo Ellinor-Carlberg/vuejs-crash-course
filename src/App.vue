c5ea04caca1e877c1dd18c9969b29bbf26a57e68
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-02-03 20:04:42 +0100

    addTodo works

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import Header from './components/layout/Header';
-import AddTodo from './components/AddTodo'
-import Todos from './components/Todos.vue';
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     AddTodo,
-    Todos
-    
+    Todos,
   },
   data() {
     return {
@@ -37,16 +36,18 @@ export default {
           title: "Todo Three",
           completed: false,
         },
-      ]
-    }
-  }, 
+      ],
+    };
+  },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter( todo => todo.id !== id);
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo]
     }
-  }
-}
-
+  },
+};
 </script>
 
 
@@ -65,5 +66,17 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
